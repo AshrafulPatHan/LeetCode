985c1ba5426ee2baafe2611ea1c046402ee12df8
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-08-06 09:17:52 +0600

    a

---
+++ test/loop.js
@@ -0,0 +1,14 @@
+
+for(let i = 0; i<5; i++){
+    console.log("hello wold");
+    
+}
+
+let count = 0; 
+
+while (count < 5) { 
+  console.log("Current count: " + count);
+  count++; 
+}
+
+console.log("Loop finished. Final count: " + count);
